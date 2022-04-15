4d610e2fd0ee66ccc763d01056b976ad07e44a41
Dan Balasescu <smoogipoo@smgi.me>
2022-04-15 17:33:12 +0900

    Fix incorrect receiver objects

--- osu.Server.Spectator.Tests/Multiplayer/MultiplayerTest.cs
+++ osu.Server.Spectator.Tests/Multiplayer/MultiplayerTest.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Moq;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Game.Online.Multiplayer;
 using osu.Server.Spectator.Database;
 using osu.Server.Spectator.Database.Models;
@@ -55,12 +56,12 @@ namespace osu.Server.Spectator.Tests.Multiplayer
         /// <summary>
         /// A receiver specific to the user with ID <see cref="USER_ID"/>.
         /// </summary>
-        protected readonly Mock<IMultiplayerClient> UserReceiver;
+        protected readonly Mock<DelegatingMultiplayerClient> UserReceiver;
 
         /// <summary>
         /// A receiver specific to the user with ID <see cref="USER_ID_2"/>.
         /// </summary>
-        protected readonly Mock<IMultiplayerClient> User2Receiver;
+        protected readonly Mock<DelegatingMultiplayerClient> User2Receiver;
 
         /// <summary>
         /// The user with ID <see cref="USER_ID"/>.
@@ -130,8 +131,8 @@ namespace osu.Server.Spectator.Tests.Multiplayer
             ContextUser2.Setup(context => context.UserIdentifier).Returns(USER_ID_2.ToString());
             ContextUser2.Setup(context => context.ConnectionId).Returns(USER_ID_2.ToString());
 
-            UserReceiver = new Mock<IMultiplayerClient>();
-            User2Receiver = new Mock<IMultiplayerClient>();
+            UserReceiver = new Mock<DelegatingMultiplayerClient>();
+            User2Receiver = new Mock<DelegatingMultiplayerClient>();
 
             Receiver = new Mock<DelegatingMultiplayerClient>(getClientsForGroup(ROOM_ID, false)) { CallBase = true };
             GameplayReceiver = new Mock<DelegatingMultiplayerClient>(getClientsForGroup(ROOM_ID, true)) { CallBase = true };
