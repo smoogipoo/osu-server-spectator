cc4714acefc8603ad2c969916092747929c06d28
Dan Balasescu <smoogipoo@smgi.me>
2025-10-21 09:13:26 +0900

    Add tag to queued users gauge

--- osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/Queue/MatchmakingQueueBackgroundService.cs
+++ osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/Queue/MatchmakingQueueBackgroundService.cs
@@ -171,15 +171,15 @@ namespace osu.Server.Spectator.Hubs.Multiplayer.Matchmaking.Queue
             if (DateTimeOffset.Now - lastLobbyUpdateTime < lobby_update_rate)
                 return;
 
-            MatchmakingQueueUser[] users = poolQueues.Values.SelectMany(queue => queue.GetAllUsers()).ToArray();
-            DogStatsd.Gauge($"{statsd_prefix}.queue.users", users.Length);
+            foreach ((_, MatchmakingQueue queue) in poolQueues)
+                DogStatsd.Gauge($"{statsd_prefix}.queue.users", queue.GetAllUsers().Length, tags: [$"queue:{queue.Pool.name}"]);
 
+            MatchmakingQueueUser[] users = poolQueues.Values.SelectMany(queue => queue.GetAllUsers()).ToArray();
             Random.Shared.Shuffle(users);
-            int[] usersSample = users.Take(50).Select(u => u.UserId).ToArray();
 
             await hub.Clients.Group(lobby_users_group).SendAsync(nameof(IMatchmakingClient.MatchmakingLobbyStatusChanged), new MatchmakingLobbyStatus
             {
-                UsersInQueue = usersSample
+                UsersInQueue = users.Take(50).Select(u => u.UserId).ToArray()
             });
 
             lastLobbyUpdateTime = DateTimeOffset.Now;
