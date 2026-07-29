41415701c8de44d8a7c50dd2574d900a544351e2
Dan Balasescu <smoogipoo@smgi.me>
2026-07-29 21:23:37 +0200

    Add room info

--- osu.Server.Spectator/Hubs/Arcade/ArcadeHub.cs
+++ osu.Server.Spectator/Hubs/Arcade/ArcadeHub.cs
@@ -1,29 +1,40 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using osu.Game.Arcade;
+using osu.Game.Online.Multiplayer;
 using osu.Server.Spectator.Database;
 using osu.Server.Spectator.Entities;
 using osu.Server.Spectator.Extensions;
+using osu.Server.Spectator.Hubs.Multiplayer;
 
 namespace osu.Server.Spectator.Hubs.Arcade
 {
     public class ArcadeHub : StatefulUserHub<IArcadeClient, ArcadeClientState>, IArcadeServer
     {
         private readonly IDatabaseFactory dbFactory;
+        private readonly EntityStore<ServerMultiplayerRoom> roomStore;
 
-        public ArcadeHub(ILoggerFactory loggerFactory, EntityStore<ArcadeClientState> userStates, IDatabaseFactory dbFactory)
+        public ArcadeHub(ILoggerFactory loggerFactory, EntityStore<ArcadeClientState> userStates, IDatabaseFactory dbFactory,
+                         EntityStore<ServerMultiplayerRoom> roomStore)
             : base(loggerFactory, userStates)
         {
             this.dbFactory = dbFactory;
+            this.roomStore = roomStore;
         }
 
         public async Task<ArcadeUserStats[]> FetchLeaderboard()
         {
             using (var db = dbFactory.GetInstance())
                 return await db.GetArcadeUserStatsAsync();
+        }
+
+        public Task<MultiplayerRoom[]> GetActiveRooms()
+        {
+            return Task.FromResult(roomStore.GetAllEntities().Select(e => e.Value.TakeSnapshot()).ToArray());
         }
 
         public async Task Connect(ArcadeIdentity identity)
