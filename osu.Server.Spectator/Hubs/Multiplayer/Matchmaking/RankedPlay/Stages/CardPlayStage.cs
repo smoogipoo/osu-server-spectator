fb1098abff3d03a54560b0873dd5821b169b579b
Dan Balasescu <smoogipoo@smgi.me>
2026-08-01 22:57:29 +0200

    Attempt delay

--- osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/RankedPlay/Stages/CardPlayStage.cs
+++ osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/RankedPlay/Stages/CardPlayStage.cs
@@ -54,7 +54,11 @@ namespace osu.Server.Spectator.Hubs.Multiplayer.Matchmaking.RankedPlay.Stages
                 return;
 
             playedCard = card;
-            await Finish();
+
+            if (card.Mystery)
+                await FinishWithCountdown(TimeSpan.FromSeconds(15));
+            else
+                await Finish();
         }
 
         public override Task HandleCardHandReplayRequest(MultiplayerRoomUser user, RankedPlayCardHandReplayRequest request)
